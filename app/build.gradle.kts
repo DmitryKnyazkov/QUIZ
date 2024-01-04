1f51a54d3e6b87013f7b0780a0ac9bb9bd7160c6
Ilya Zhigaras <zhigaras.dev@gmail.com>
2024-01-04 20:42:20 +0600

    fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.quiz"
+        applicationId = "com.quizzz"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
